591d2b2e949e54e0ab6a15e982f3feffb2e03d55
tajweb <tajweb.m@gmail.com>
2023-07-31 18:45:41 +0500

    refactor

--- main.js
+++ main.js
@@ -10,18 +10,31 @@ let nextId = 1;
 const posts = [];
 
 const methods = new Map();
-methods.set('/posts.get', function(request, response) {
-    response.writeHead(statusOk, {'Comtent-Type': 'application/json'});
-    response.end(JSON.stringify(posts));
+
+function sendResponse(response, {status = statusOk, headers = {}, body = null}) {
+    Object.entries(headers).forEach(function([key, value]) {
+        response.setHeader(key, value);
+    });
+    response.writeHead(status);
+    response.end(body);
+}
+
+function sendJSON(response, body) {
+    sendResponse(response, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+    });
+}
+
+methods.set('/posts.get', function({response}) {
+    sendJSON(response, posts);
 });
-methods.set('/posts.getById', function(request, response) {});
-methods.set('/posts.post', function(request, response) {
-    const url = new URL(request.url, `http://${request.headers.host}`);
-    const searchParams = url.searchParams;
-    
+methods.set('/posts.getById', function({}) {});
+methods.set('/posts.post', function({response, searchParams}) {    
     if (!searchParams.has('content')) {
-        response.writeHead(statusBadRequest);
-        response.end();
+        sendResponse(response, {status: statusBadRequest});
         return;
     }
 
@@ -34,24 +47,29 @@ methods.set('/posts.post', function(request, response) {
     };
 
     posts.unshift(post);
-    response.writeHead(statusOk, {'Content-Type': 'application/json'});
-    response.end(JSON.stringify(post));
+    sendJSON(response, post);
 });
 
-methods.set('/posts.edit', function(request, response) {});
-methods.set('/posts.delete', function(request, response) {});
+methods.set('/posts.edit', function({}) {});
+methods.set('/posts.delete', function({}) {});
 
 const server = http.createServer(function (request, response) {
-    const url = new URL(request.url, `http://${request.headers.host}`);
-    const pathname = url.pathname;
+    const {pathname, searchParams} = new URL(request.url, `http://${request.headers.host}`);
+    
     const method = methods.get(pathname);
     if (method === undefined) {
-        response.writeHead(StatusNotFound);
-        response.end();
+        sendResponse(response, {status: StatusNotFound});
         return;
     }
 
-    method(request, response);
+    const params = {
+        request,
+        response,
+        pathname,
+        searchParams,
+    };
+
+    method(params);
 });
 
 const port = 9999;
